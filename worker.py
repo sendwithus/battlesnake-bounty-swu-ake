927a2897d3af446d5f0eeadb701691242bb76e2c
John Oram <john@sendwithus.com>
2016-02-08 14:03:54 -0800

    consider quality

--- worker.py
+++ worker.py
@@ -15,22 +15,25 @@ def clear_redis():
     	print "nuking: %s" % key
     	redis_server.delete(key)
 
-def visit(game, data):
-	print "visiting %s" % data
+def visit(game, data, redis_key):
 	board = RedisBoard(payload=json.loads(data))
+	redis_server.add("%s_quality" % redis_key, board.quality())
+
+	# visit children
 	for child_payload in board.children_payloads():
 		child_payload = json.dumps(child_payload)
 		if not redis_server.exists(child_payload):
-			redis_server.sadd("%s_to_visit" % game, child_payload)
-			print child_payload
+			redis_server.sadd(redis_key, child_payload)
+
 
 clear_redis()
 test()
 
 while True:
 	for game in redis_server.smembers("active_games"):
-		board_key = redis_server.spop("%s_to_visit" % game)
-		if game and board_key:
-			visit(game, board_key)
-		time.sleep(1)
+		for redis_key in ["%s_N" % game, "%s_S" % game, "%s_E" % game,"%s_W" % game]:
+			board_key = redis_server.spop(redis_key)
+			if game and board_key:
+				visit(game, board_key, redis_key)
+			time.sleep(1)
 
