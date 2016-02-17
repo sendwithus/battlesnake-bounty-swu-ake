66ced24d837c148bf4f4ee7b54e452de3ed999ac
John Oram <john@sendwithus.com>
2016-02-17 10:04:23 -0800

    tweak

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import json
+import redis
 import settings
 
 _redis_server = None
@@ -10,21 +12,23 @@ def redis_server():
 
 	url = "h:p3e5ialsr72o65caic95m4icq3b@ec2-54-235-152-160.compute-1.amazonaws.com"
 	port = 23619
-	_redis_server = redis.Redis(url, port)
+	_redis_server = redis.Redis(host=url, port=port)
 	return _redis_server
 
 def redis_key(payload):
-	# consistent order snakes
-	snakes = {}
-	for snake in payload.get("snakes", {}):
-		snakes[snake.get("name")] = snake
-	snake_ids = snakes.keys()
-	snake_ids.sort()
-	snakes = [snakes[key] for key in snake_ids]
+	return json.dumps(payload)
 
-	key = payload.get("game")
-	for snake in snakes:
-		key += "_"
-		for x, y in snake.get("coords", []):
-			key += "%d%d" % (x, y)
-	return key
+	# # consistent order snakes
+	# snakes = {}
+	# for snake in payload.get("snakes", {}):
+	# 	snakes[snake.get("name")] = snake
+	# snake_ids = snakes.keys()
+	# snake_ids.sort()
+	# snakes = [snakes[key] for key in snake_ids]
+
+	# key = payload.get("game")
+	# for snake in snakes:
+	# 	key += "_"
+	# 	for x, y in snake.get("coords", []):
+	# 		key += "%d%d" % (x, y)
+	# return key
