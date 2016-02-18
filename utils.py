04c64ba1c417f21c0921f1dcd46dd652f2c82509
John Oram <john@sendwithus.com>
2016-02-18 10:38:27 -0800

    redis key string

--- utils.py
+++ utils.py
@@ -11,16 +11,26 @@ def redis_server():
 	if _redis_server:
 		return _redis_server
 
-	# url, port = settings.REDIS_URL.rsplit(':', 1)
-	# port = int(port)
-	# print "%s, %s" % (url, port)
-	# _redis_server = redis.from_url(url, port)
-
 	_redis_server = redis.from_url(os.environ.get("REDIS_URL"))
 	return _redis_server
 
 def redis_key(payload):
-	return json.dumps(payload)
+	# consistent order snakes
+	snakes = {}
+	for snake in payload.get("snakes", {}):
+		snakes[snake.get("name")] = snake
+	snake_ids = snakes.keys()
+	snake_ids.sort()
+	snakes = [snakes[key] for key in snake_ids]
+
+	# list of snakes
+	key = payload.get("game")
+	for snake in snakes:
+		key += "_"
+		for x, y in snake.get("coords", []):
+			key += "%d%d" % (x, y)
+
+	return key
 
 def add_vectors(v1, v2):
 	(x1, y1) = v1
@@ -33,17 +43,3 @@ def subtract_vectors(v1, v2):
 	return x1-x2, y1-y2
 
 
-	# # consistent order snakes
-	# snakes = {}
-	# for snake in payload.get("snakes", {}):
-	# 	snakes[snake.get("name")] = snake
-	# snake_ids = snakes.keys()
-	# snake_ids.sort()
-	# snakes = [snakes[key] for key in snake_ids]
-
-	# key = payload.get("game")
-	# for snake in snakes:
-	# 	key += "_"
-	# 	for x, y in snake.get("coords", []):
-	# 		key += "%d%d" % (x, y)
-	# return key
