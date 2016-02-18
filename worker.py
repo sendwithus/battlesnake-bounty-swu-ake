dda2bf3e308c963b2c040f552f2f0c76762da738
John Oram <john@sendwithus.com>
2016-02-18 10:43:17 -0800

    debugging

--- worker.py
+++ worker.py
@@ -10,15 +10,15 @@ from utils import redis_server
 
 
 def visit(game, data, redis_key):
+	print "visiting %s" % redis_key
 	board = RedisBoard(payload=json.loads(data))
 	redis_server().add("%s_quality" % redis_key, board.quality())
-	print "visiting %s" % redis_key
 
-	# visit children
-	for child_payload in board.children_payloads():
-		child_payload = json.dumps(child_payload)
-		if not redis_server().exists(child_payload):
-			redis_server().sadd(redis_key, child_payload)
+	# # visit children
+	# for child_payload in board.children_payloads():
+	# 	child_payload = json.dumps(child_payload)
+	# 	if not redis_server().exists(child_payload):
+	# 		redis_server().sadd(redis_key, child_payload)
 
 while True:
 	for game in redis_server().smembers("active_games"):
@@ -26,4 +26,6 @@ while True:
 			board_key = redis_server().spop(redis_key)
 			if game and board_key:
 				visit(game, board_key, redis_key)
+			time.sleep(0)
+		time.sleep(0)
 	time.sleep(0)
