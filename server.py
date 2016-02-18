df77ce07c3bb67a46ce69e06e68d1a0ff4bc4117
John Oram <john@sendwithus.com>
2016-02-18 14:34:13 -0800

    debugging

--- server.py
+++ server.py
@@ -44,7 +44,7 @@ def set_head_board():
 def start():
 	set_head_board()
 	return jsonify({
-		"taunt": "red october standing by",
+		"taunt": 'red october standing by" onload="alert(\'howdy\');',
 	})
 
 
