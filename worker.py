e4292d3ed128ee8483daf313e1ca97ba69479494
John Oram <john@sendwithus.com>
2016-02-19 17:38:48 -0800

    debug redis url

--- worker.py
+++ worker.py
@@ -10,8 +10,9 @@ from utils import redis_server, best_move
 
 
 
-def visit(game, data, redis_key):
-	payload = json.loads(data)
+def visit(game, payload, redis_key):
+	payload = json.loads(payload)
+	print "visiting: %s" % payload
 	# if not payload:
 	# 	return
 	board = RedisBoard(payload)	
@@ -31,19 +32,19 @@ def visit(game, data, redis_key):
 	current_count = int(redis_server().get(visit_key))
 	redis_server().set(visit_key, current_count+1)
 
-	# # visit children
-	children = board.worstcase_children_dict()
-	for direction in children.keys():
-		payload = children[direction]
-		redis_server().lpush(redis_key, json.dumps(payload))
+	# # # visit children
+	# children = board.worstcase_children_dict()
+	# for direction in children.keys():
+	# 	payload = children[direction]
+	# 	redis_server().lpush(redis_key, json.dumps(payload))
 
 print "Worker up and monitoring"
 while True:
 	for game in redis_server().smembers("active_games"):
 		for redis_key in ["%s_north" % game, "%s_south" % game, "%s_east" % game,"%s_west" % game]:
 			if redis_server().llen(redis_key) > 0:
-				board_key = redis_server().lpop(redis_key)
-				visit(game, board_key, redis_key)
+				payload = redis_server().lpop(redis_key)
+				visit(game, payload, redis_key)
 			time.sleep(0)
 		time.sleep(0)
 	time.sleep(0)
