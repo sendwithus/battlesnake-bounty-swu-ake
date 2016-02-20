fa558849a0479eb1a9edf4874e325c071432617a
John Oram <john@sendwithus.com>
2016-02-19 23:55:43 -0800

    consider time

--- server.py
+++ server.py
@@ -118,7 +118,7 @@ def move():
 	move = "south"
 	try:
 		board = set_head_board()
-		time.sleep(0.9)
+		time.sleep(0.8)
 		data = json.loads(request.data)
 		game = data.get("game")
 		move = best_move(data.get("game"), board)
