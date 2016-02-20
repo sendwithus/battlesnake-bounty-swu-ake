5c5f8f23df7bb39dd97d1d452e0348a35df2b9c7
John Oram <john@sendwithus.com>
2016-02-19 17:18:38 -0800

    debug redis url

--- worker.py
+++ worker.py
@@ -43,6 +43,7 @@ while True:
 		for redis_key in ["%s_north" % game, "%s_south" % game, "%s_east" % game,"%s_west" % game]:
 
 			board_key = redis_server().spop(redis_key)
+			print "got %s off of %s" % (board_key, redis_key)
 			# if game and board_key:
 			# 	visit(game, board_key, redis_key)
 			time.sleep(0)
