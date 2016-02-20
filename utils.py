14fa1ee2448c5bddb5ef31f1697abd64e1f7a742
John Oram <john@sendwithus.com>
2016-02-19 21:18:32 -0800

    debug redis url

--- utils.py
+++ utils.py
@@ -43,21 +43,25 @@ def subtract_vectors(v1, v2):
 	return x1-x2, y1-y2
 
 def best_move(game, board):
-	n = redis_server().get("%s_north_quality" % game)
-	s = redis_server().get("%s_south_quality" % game)
-	e = redis_server().get("%s_east_quality" % game)
-	w = redis_server().get("%s_west_quality" % game)
-	qualities = [
-		('north', int(n) if n != 'None' else 0),
-		('south', int(s) if s != 'None' else 0),
-		('east', int(e) if e != 'None' else 0),
-		('west', int(w) if w != 'None' else 0),
-	]
-	print "qualities: %s" % qualities
-	qualities = sorted(qualities, key=lambda tup: tup[1])
-	valid_moves = board.valid_moves()
-	print "qualities: %s" % qualities
-	for (direction, weight) in qualities:
-		if direction in valid_moves:
-			print "best: %s" % direction
-			return direction
+	try:
+		n = redis_server().get("%s_north_quality" % game)
+		s = redis_server().get("%s_south_quality" % game)
+		e = redis_server().get("%s_east_quality" % game)
+		w = redis_server().get("%s_west_quality" % game)
+		qualities = [
+			('north', int(n) if n != 'None' else 0),
+			('south', int(s) if s != 'None' else 0),
+			('east', int(e) if e != 'None' else 0),
+			('west', int(w) if w != 'None' else 0),
+		]
+		print "qualities: %s" % qualities
+		qualities = sorted(qualities, key=lambda tup: tup[1])
+		valid_moves = board.valid_moves()
+		print "qualities: %s" % qualities
+		for (direction, weight) in qualities:
+			if direction in valid_moves:
+				print "best: %s" % direction
+				return direction
+	except Exception as e:
+		print e
+		return "north"
